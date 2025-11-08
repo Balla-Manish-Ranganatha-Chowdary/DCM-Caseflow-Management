0d39597b402f0e502c9a6c550fead2b4b34ea0a2
Ruthvik-09 <ruthvikv9@gmail.com>
2025-11-08 23:58:22 +0530

    added css to userloginpage

--- Front-End/src/pages/UserLoginPage.jsx
+++ Front-End/src/pages/UserLoginPage.jsx
@@ -8,28 +8,34 @@ export function UserLoginPage(){
             <div className="sign-in-format">
                 <h1>Sign-in</h1>
                 <div className="alernative-login">
-                    <a href="gmail.com" target="_blank">
-                        <img src="../src/assets/mail.png" width="50px" />
+                    <a href="gmail.com" target="_blank" className="login-image">
+                        <img src="../src/assets/mail.png" width="40px" className="logo" />
                     </a>
-                    <a href="facebook.com" target="_blank">
-                        <img src="../src/assets/facebook.png" width="50px"/>
+                    <a href="facebook.com" target="_blank" className="login-image">
+                        <img src="../src/assets/facebook.png" width="40px" className="logo" />
                     </a>
-                    <a href="github.com" target="_blank">
-                        <img src="../src/assets/github.png" width="50px" />
+                    <a href="github.com" target="_blank" className="login-image">
+                        <img src="../src/assets/github.png" width="40px" className="logo" />
                     </a>
-                    <a href="linkedin.com" target="_blank">
-                        <img src="../src/assets/linked.png" width="50px" />
+                    <a href="linkedin.com" target="_blank" className="login-image">
+                        <img src="../src/assets/linkedin.png" width="40px" className="logo" />
                     </a>
                 </div>
                 <div className="login-form">
-                    <h2>Use your email password</h2>
+                    <p className="p-sentence">
+                        Use your email password
+                    </p>
                     <form action="/login">
                         <input type="email" className="email-form" placeholder="Email" />
-                        <input type="password" className="password-form" placeholder="password" />
-                        <div>
-                            <Link to="forget-pass">Forget your password</Link>
+                        <input type="password" className="password-form" placeholder="Password" />
+                        <div className="forgot-container">
+                            <Link to="forget-pass" className="forgot-link">
+                                Forget your password?
+                            </Link>
                         </div>
-                        <button type="submit">Sign In</button>
+                        <button type="submit" className="sign-in-btn">
+                            Sign In
+                        </button>
                     </form>  
                 </div>
             </div> 
